dddea85109dd7c28e8aa8e54567dec4901c00b3d
Emmanuel Pericon <brianpericon.e92@gmail.com>
2019-11-16 14:51:05 -0300

    Cambios en metodos para aceptar/rechazar postulante

--- app-backend/controller/solicitud.controller.js
+++ app-backend/controller/solicitud.controller.js
@@ -1,5 +1,7 @@
-const Publication = require('../model/db/publication.db');
-const ApiResponse = require('../model/api.response');
+const 
+  Publication = require('../model/db/publication.db'),
+  ApiResponse = require('../model/api.response'),
+  Application = require('../model/db/application.db');
 
 const solicitudCtrl = {};
 
@@ -44,30 +46,46 @@ async function requestSolicitudes(query, req, res) {
 solicitudCtrl.putAceptarSolicitante = async (req, res) => {
 
   const { id } = req.params; //application id
-  const { idPublicacion } = req.body;
+  const { idPublicacion } = req.body.idPublicacion;
 
-  const pub = await Publication.updateOne(
-    { _id: idPublicacion, "applications._id": id },
-    { $set: { "applications.$.status": "aceptado" } }
-  )
+  // const pub = await Publication.updateOne(
+  //   { _id: idPublicacion, "applications._id": id },
+  //   { $set: { "applications.$.status": "aceptado" } }
+  // )
+  try {
+    await Application.updateOne({ _id: id }, { $set: { status: 'aceptado' } }, { new: true });
+  } catch (e) {
+    return res.json(new ApiResponse('Error al aceptar usuario', 400, {}, e));
+  }
 
-  res.json(new ApiResponse('Usuario aceptado', 200, pub));
+  const retPublication = await Publication.findById(idPublicacion)
+    .populate({ path: 'pet', model: 'Pet', populate: { path: 'user', model: 'User' } })
+    .populate({ path: 'applications', model: 'Application', populate: { path: 'user', model: 'User' } });
+
+  res.json(new ApiResponse('Usuario aceptado', 200, retPublication));
 };
 
 solicitudCtrl.putRechazarSolicitante = async (req, res) => {
 
   const { id } = req.params;
-  const { idPublicacion } = req.body;
+  const { idPublicacion } = req.body.idPublicacion;
 
-  const pub = await Publication.updateOne(
-    { _id: idPublicacion, "applications._id": id },
-    { $set: { "applications.$.status": "rechazado" } }
-  )
+  // const retPublication = await Publication.updateOne(
+  //   { _id: idPublicacion, 'applications._id': id },
+  //   { $set: { 'applications.$.status': 'rechazado' } }
+  // )
 
-  res.json(new ApiResponse('Usuario rechazado', 200, pub));
+  try {
+    await Application.updateOne({ _id: id }, { $set: { status: 'rechazado' } }, { new: true });
+  } catch (e) {
+    return res.json(new ApiResponse('Error al rechazar usuario', 400, {}, e));
+  }
+
+  const retPublication = await Publication.findById(idPublicacion)
+    .populate({ path: 'pet', model: 'Pet', populate: { path: 'user', model: 'User' } })
+    .populate({ path: 'applications', model: 'Application', populate: { path: 'user', model: 'User' } });
+
+  res.json(new ApiResponse('Usuario rechazado', 200, retPublication));
 };
-
-
-
 
 module.exports = solicitudCtrl;
